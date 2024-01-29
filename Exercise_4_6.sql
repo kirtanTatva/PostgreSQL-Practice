207981789cb3f40ac62b188fa4de4ca6633d664e
kirtankp <kirtankp1902@gmail.com>
2024-01-29 10:44:16 +0530

    Exercise 4 6 done

--- Exercise_4_6.sql
+++ Exercise_4_6.sql
@@ -1,3 +1,4 @@
+--4
 SELECT title, replacement_cost, rental_duration
 FROM film
 WHERE rental_duration BETWEEN 4 AND 6
@@ -5,9 +6,15 @@ ORDER BY replacement_cost DESC
 LIMIT 100;
 
 
-
 SELECT title,description,length,rating 
 FROM film
 WHERE length > 120
 AND rating IN ('G','PG')
 AND description LIKE '%Action%';
+
+
+
+--6
+SELECT first_name,count(*)
+FROM actor
+GROUP BY first_name;
