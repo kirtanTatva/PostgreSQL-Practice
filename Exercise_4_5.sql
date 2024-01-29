0ad91561b0f7f7d5adf96e02bae426eabaa251fa
kirtankp <kirtankp1902@gmail.com>
2024-01-29 10:00:59 +0530

    Exercise 4 5 added

---
+++ Exercise_4_5.sql
@@ -0,0 +1,13 @@
+SELECT title, replacement_cost, rental_duration
+FROM film
+WHERE rental_duration BETWEEN 4 AND 6
+ORDER BY replacement_cost DESC
+LIMIT 100;
+
+
+
+SELECT title,description,length,rating 
+FROM film
+WHERE length > 120
+AND rating IN ('G','PG')
+AND description LIKE '%Action%';
